2ab6ff24561d7505e6ac1bdd4518a7345f1cd770
Anas Khan <anas.khan96@outlook.com>
2018-03-18 04:27:49 +0530

    reading history on startup

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -37,8 +36,10 @@ func sesh_loop() {
 	CWD = wdSlice[len(wdSlice)-1]
 
 	history := make([]string, 0)
+	history = initHistory(history)
 	status, historyCount := 1, 0
 	reader := bufio.NewReader(os.Stdin)
+	//go listenForKeyPress()
 
 	for status != 0 {
 		symbol := "\u2713"
@@ -46,11 +47,8 @@ func sesh_loop() {
 			symbol = "\u2715"
 		}
 		fmt.Printf("sesh 🔥  %s %s ", CWD, symbol)
-		line, err := reader.ReadString('\n')
+		line, _ := reader.ReadString('\n')
 		line = line[:len(line)-1]
-		if err != nil {
-			log.Fatalf("sesh: %s\n", err.Error())
-		}
 		args := splitIntoTokens(line)
 		status = execute(args)
 		if status == 1 {
@@ -102,11 +100,31 @@ func execute(args []string) int {
 	return launch(args)
 }
 
+func initHistory(history []string) []string {
+	if _, err := os.Stat(HISTFILE); err == nil {
+		f, _ := os.OpenFile(HISTFILE, os.O_RDONLY, 0666)
+		defer f.Close()
+		/* Read file and store each line in history slice */
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			text := scanner.Text()
+			history = append(history, text)
+		}
+		// Reversing the history slice
+		last := len(history) - 1
+		for i := 0; i < len(history)/2; i++ {
+			history[i], history[last-i] = history[last-i], history[i]
+		}
+	}
+	return history
+}
+
 func exit() {
-	f, err := os.OpenFile(".sesh_history", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	f, err := os.OpenFile(HISTFILE, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
 	if err != nil {
 		fmt.Printf(ERRFORMAT, err.Error())
 	}
+	defer f.Close()
 	for _, i := range HISTMEM {
 		f.Write([]byte(i))
 		f.Write([]byte("\n"))
