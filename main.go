ea3cde9d7c43b09a0681d08797b6481e454edefd
Anas Khan <anas.khan96@outlook.com>
2018-03-17 22:02:46 +0530

    receiving lines

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
+const TOKDELIM = " \t\r\n\a"
+
+var CWD string
+
+func main() {
+	/* Importing config */
+
+	sesh_loop()
+
+	os.Exit(0)
+}
+
+func sesh_loop() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	wdSlice := strings.Split(wd, "/")
+	CWD = wdSlice[len(wdSlice)-1]
+
+	reader := bufio.NewReader(os.Stdin)
+	status := true
+
+	for status {
+		fmt.Printf("sesh 🔥 %s -> ", CWD)
+		line, _ := reader.ReadString('\n')
+		args := strings.Split(line, TOKDELIM)
+		status = execute(args)
+	}
+}
+
+func execute(args []string) bool {
+	return false
 }
